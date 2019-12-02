51df6dae2b622bd9bfca10495769192323619c08
Gavin <gwu23@uic.edu>
2019-12-02 16:35:00 -0600

    structure changed

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,21 +4,13 @@ import './index.css';
 
 class Square extends React.Component {
 
-  // constructor
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
-
   render() {
     return (
       <button
         className="Square"
-        onClick={() => this.setState ({value: 'X'})}
+        onClick={() => this.props.onClick()}
         >
-        {this.state.value}
+        {this.props.value}
       </button>
     );
   }
@@ -27,9 +19,23 @@ class Square extends React.Component {
 class Board extends React.Component {
 
   // make a constructor
-  
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill (null),
+    };
+  }
+
+  handleClick(i) {
+  const squares = this.state.squares.slice();
+  squares[i] = 'X';
+  this.setState({squares: squares});
+}
+
   renderSquare(i) {
-    return <Square value ={i} />;
+    return <Square value ={this.state.squares[i]}
+    onClick={()=> this.handleClick(i)}
+     />;
   }
 
   render() {
